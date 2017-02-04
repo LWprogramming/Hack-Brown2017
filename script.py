fb5a0d0cf9e3e14e418b06c373f26a2e6e2a7c1e
Leon Wu <lwgithub@gmail.com>
2017-02-04 14:49:23 -0500

    Read data from csv
    Add code to read data from the csv file and load into a numpy nd-array.

---
+++ script.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas
+
+def main():
+	data = pandas.read_csv('sarcasm_v2.csv').as_matrix()
+	# print(data.shape)
+	data[:, 0] = np.array([find_category(x) for x in data[:, 0]])
+	data[:, 1] = np.array([sarcasm(x) for x in data[:, 1]])
+	# print(data[0,1]) # should be 1 for sarcasm
+
+def find_category(category):
+	return {
+        'GEN': 0, # general
+        'HYP': 1, # hyperbole
+		'RQ': 2 # rhetorical question
+    }[category]
+
+def sarcasm(sarcasm_value):
+	return {
+		'sarc': 1, # true for sarcasm
+		'notsarc': 0 # false for sarcasm
+	}[sarcasm_value]
+
+def get_data_index(ID):
+	'''find the index of the data point. Corresponds to 1234 in GEN_sarc_1234 under ID in data.
+	'''
+	# TODO
+
+if __name__ == '__main__':
+	main()
