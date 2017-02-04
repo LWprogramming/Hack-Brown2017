0926a295327e79c56f607b0388ca7ec57d3d14cf
Leon Wu <lwgithub@gmail.com>
2017-02-04 14:57:22 -0500

    Add dataset source to script

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
+'''
+Dataset source: https://nlds.soe.ucsc.edu/sarcasm2
+
+By Shereen Oraby, Vrindavan Harrison, Lena Reed, Ernesto Hernandez, Ellen Riloff and Marilyn Walker. "Creating and Characterizing a Diverse Corpus of Sarcasm in Dialogue." In The 17th Annual SIGdial Meeting on Discourse and Dialogue (SIGDIAL), Los Angeles, California, USA, 2016.
+'''
 import numpy as np
 import pandas
 
