327711eb6e2ae1677c8040db262b93fd733aa32c
Leon Wu <lwgithub@gmail.com>
2017-02-05 11:29:55 -0500

    Create classifier

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import data_clean
 import request
 import re
 import numpy as np
+from sklearn import SGDClassifier
 
 '''
 Model for determining whether a given piece of input text is sarcastic or not.
@@ -24,9 +25,22 @@ def main():
 	reduced_data = data[0:2, :] # take the first 20 examples
 	reduced_quotes = reduced_data[:, 3]
 	reduced_response = reduced_data[:, 4]
-	save_training_data(reduced_data[:, 1], reduced_quotes, reduced_response)
+	save_data(reduced_data[:, 1], reduced_quotes, reduced_response, 'training_data')
+	classifier = train()
 
-def save_training_data(labels, quotes_vector, response_vector):
+	clf = SGDClassifier(loss="hinge", penalty="l2")
+	
+
+def train():
+	file_name = 'training_data.csv'
+	data = pandas.read_csv('sarcasm_v2.csv').as_matrix()
+	labels = data[:, 0]
+	features = data[:, 1:]
+	clf = SGDClassifier(loss="hinge", penalty="l2")
+	clf.fit(features, labels)
+	return clf
+
+def save_data(labels, quotes_vector, response_vector, file_name):
 	'''creates a csv with some number of rows with 3 columns--the first being the label for the training example, the second as the sentiment value for the quote, and the third being the sentiment value for the response. Saves the csv file.
 	'''
 	num_examples = labels.size # should be same as quotes or response vector length
@@ -36,10 +50,23 @@ def save_training_data(labels, quotes_vector, response_vector):
 	response_sentiment_vector = np.array(re.findall('0\.[0-9]+', response_sentiment_vector))
 	quote_sentiment_vector = np.array([float(x) for x in quote_sentiment_vector])
 	response_sentiment_vector = np.array([float(x) for x in response_sentiment_vector]) # change strings to numbers
-	data = np.concatenate((labels.reshape(num_examples, 1), quote_sentiment_vector.reshape(num_examples, 1), response_sentiment_vector.reshape(num_examples, 1)), axis=1)
+	labels = labels.reshape(num_examples, 1)
+	quote_sentiment_vector = quote_sentiment_vector.reshape(num_examples, 1)
+	response_sentiment_vector = response_sentiment_vector.reshape(num_examples, 1)
+
+	# TODO: fix this
+	# # now adding polynomial features of up to degree 5.
+	# features = np.zeros((num_examples, 10))
+	# for i in range(5):
+	# 	features[i] = quote_sentiment_vector ** i
+	# 	features[10 - i - 1] = response_sentiment_vector ** i
+	# data = np.concatenate((labels, quote_sentiment_vector, response_sentiment_vector, features), axis=1)
+
+	data = np.concatenate((labels, quote_sentiment_vector, response_sentiment_vector), axis=1)
 	data = data.reshape(data.size / 3, 3)
-	np.savetxt('training_data.csv', data, delimiter=',')
-	return data
+	print(data)
+	np.savetxt(file_name + '.csv', data, delimiter=',')
+
 
 if __name__ == "__main__":
 	main()
