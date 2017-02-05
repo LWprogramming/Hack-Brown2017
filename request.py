f75e2aeec7b35bc45695a16e157fd57e4013fd89
Leon Wu <lwgithub@gmail.com>
2017-02-04 22:45:04 -0500

    Modify import of data_clean from former file name "script"

--- request.py
+++ request.py
@@ -2,7 +2,7 @@
 Code adapted from https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
 '''
 import http.client, urllib.request, urllib.parse, urllib.error
-import script
+import data_clean
 import numpy as np
 
 def main():
